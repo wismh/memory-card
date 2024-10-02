a9047c4ab3ede2003eb39fcf28341f9d2b0900e3
NeutrinoZh <neutrinozh@gmail.com>
2024-10-02 14:15:20 +0200

    mc-001 chore: setup project

---
+++ Assets/_Project/Scripts/GamePlay/Board/BoardInstaller.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using Zenject;
+
+public class BoardInstaller : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+    }
+}
---
+++ Assets/_Project/Scripts/GamePlay/GamePlayInstaller.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using Zenject;
+
+public class GamePlayInstaller : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+    }
+}
